a829e0ab3db4790d957b909613f78aa85fd666f6
Sylvestre <sylestre.cao@gmail.com>
2017-01-14 22:13:08 +0100

    add directory

--- src/LouvreBundle/Controller/LouvreController.php
+++ src/LouvreBundle/Controller/LouvreController.php
@@ -40,6 +40,7 @@ class LouvreController extends Controller
     
     public function indexAction()
     {
+        var_dump(__DIR__);
         return $this->render('LouvreBundle:Louvre:index.html.twig');
     }
 
