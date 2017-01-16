30e19fc8514b06c0244bfd29cbb1952adf9d9b94
Sylvestre <sylestre.cao@gmail.com>
2017-01-16 23:47:47 +0100

    change travis.yml

---
+++ tests/LouvreBundle/Controller/LouvreControllerTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace LouvreBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class LouvreControllerTest extends WebTestCase
+{
+
+    public function testTitle(){
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+        $this->assertCount(3, $crawler->filter('h2'));
+    }
+
+    public function testMainTitle(){
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+        $this->assertGreaterThan(0, $crawler->filter('h1')->count());
+    }
+
+    public function testClick(){
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/billeterie');
+        $link = $crawler
+            ->filter('a') // find all links with the text "Greet"
+            ->link()
+        ;
+        $crawler = $client->click($link);
+    }
+
+    public function testContent()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/billeterie');
+        $this->assertContains('Billeterie', $client->getResponse()->getContent());
+    }
+
+    public function test200All()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/');
+        $this->assertTrue($client->getResponse()->isSuccessful());
+    }
+
+    public function test404()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/randompage');
+        $this->assertTrue($client->getResponse()->isNotFound());
+    }
+    
+
+}
