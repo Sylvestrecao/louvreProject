54b8fa8ac404a2f0c31c8b6a6d2a51fd43c4fded
Sylvestre <sylestre.cao@gmail.com>
2017-02-10 17:45:04 +0100

    add unit test

---
+++ src/LouvreBundle/Util/Calculator.php
@@ -0,0 +1,35 @@
+<?php
+// src/LouvreBundle/Util/Calculator.php
+namespace LouvreBundle\Util;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ORM\EntityManager;
+
+class Calculator
+{
+    private $entityManager;
+
+    public function __construct(ObjectManager $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function calculateQuantity($id)
+    {
+        $commandeRepository = $this->entityManager->getRepository('LouvreBundle:Commandes');
+        $commande = $commandeRepository->find($id);
+        $result = $commande->getQuantite();
+        return $result;
+    }
+
+    public function getClient($id)
+    {
+        $clientRepository = $this->entityManager->getRepository('LouvreBundle:Clients');
+        $client = $clientRepository->find($id);
+        $result = $client->getNom();
+        return $result;
+    }
+
+    public function setClient(){
+     
+    }
+}
---
+++ tests/LouvreBundle/Util/CalculatorTest.php
@@ -0,0 +1,70 @@
+<?php
+namespace tests\LouvreBundle\Util;
+
+use LouvreBundle\Util\Calculator;
+use LouvreBundle\Entity\Clients;
+use LouvreBundle\Entity\Commandes;
+use Doctrine\ORM\EntityRepository;
+use Doctrine\Common\Persistence\ObjectManager;
+class CalculatorTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCalculateQuantity()
+    {
+        // First, mock the object to be used in the test
+        $commande = $this->createMock(Commandes::class);
+        $commande->expects($this->once())
+            ->method('getQuantite')
+            ->will($this->returnValue(3));
+
+        // Now, mock the repository so it returns the mock of the employee
+        $commandeRepository = $this
+            ->getMockBuilder(EntityRepository::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $commandeRepository->expects($this->once())
+            ->method('find')
+            ->will($this->returnValue($commande));
+
+        // Last, mock the EntityManager to return the mock of the repository
+        $entityManager = $this
+            ->getMockBuilder(ObjectManager::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $entityManager->expects($this->once())
+            ->method('getRepository')
+            ->will($this->returnValue($commandeRepository));
+
+        $commandeCalculator = new Calculator($entityManager);
+        $this->assertEquals(3, $commandeCalculator->calculateQuantity(1));
+    }
+
+    public function testGetClient()
+    {
+        // First, mock the object to be used in the test
+        $client = $this->createMock(Clients::class);
+        $client->expects($this->once())
+            ->method('getNom')
+            ->will($this->returnValue('John'));
+
+        // Now, mock the repository so it returns the mock of the employee
+        $clientRepository = $this
+            ->getMockBuilder(EntityRepository::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $clientRepository->expects($this->once())
+            ->method('find')
+            ->will($this->returnValue($client));
+
+        // Last, mock the EntityManager to return the mock of the repository
+        $entityManager = $this
+            ->getMockBuilder(ObjectManager::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $entityManager->expects($this->once())
+            ->method('getRepository')
+            ->will($this->returnValue($clientRepository));
+
+        $clientCalculator = new Calculator($entityManager);
+        $this->assertEquals('John', $clientCalculator->getClient(1));
+    }
+}
